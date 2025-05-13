ec6bbc77064c8a7b3b90a50401f30c33d61e13ad
Bhrenz <btbanaag0213ant@student.fatima.edu.ph>
2025-05-14 00:01:48 +0800

    Initial commit

--- db/database.py
+++ db/database.py
@@ -1,10 +1,16 @@
+import os
+
 import psycopg2
+from dotenv import load_dotenv
+
+# load env file
+load_dotenv()
 
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
-        database="Capstone",
-        user="postgres",
-        password="admin",
-        port=5432
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
     )
